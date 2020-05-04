7394d15ddc46073cdf893f7caea46c39ae5785ff
DearYanyan22 <DearYanyan22@users.noreply.github.com>
2020-05-04 00:48:15 -0400

    add three modes

--- js/index.js
+++ js/index.js
@@ -2,31 +2,77 @@
 
 let level = 11 
 let score = 0
-
-
+let scoreshow = 0
+var step 
 var display = document.querySelector("#display")
 var levelDisplay = document.querySelector("#level")
 var start = document.querySelector("#start")
+var begindisplay = document.querySelector("#begindisplay")
 var squares = document.querySelectorAll('.square')
+var modes = document.querySelectorAll('.mode')
 var resultdisplay = document.querySelector("#resultdisplay")
+var scoredisplay = document.querySelector("#scoreDisplay")
 var resultdiv = document.querySelector(".result-div")
-var imgdisplay = document.querySelector("#imgdisplay")
-var step = 1
 var voice1 = document.createElement("audio");
 var voice2 = document.createElement("audio");
 var voice3 = document.createElement("audio");
 var voice4 = document.createElement("audio");
-  voice1.src = "game.m4a";
-  voice2.src = "win.m4a";
-  voice3.src = "beginwin.m4a";
-  voice4.src = "finalwin.m4a";
+  voice1.src = "asset/game.m4a";
+  voice2.src = "asset/win.m4a";
+  voice3.src = "asset/beginwin.m4a";
+  voice4.src = "asset/finalwin.m4a";
 
 
-window.addEventListener("load",init())
+//window.addEventListener("load",init())
+
+for(var i = 0; i < modes.length; i++) {
+
+	    modes[i].addEventListener("click", function() {
+
+		display.innerHTML = "Ready? Go!"
+		begindisplay.style.display = "none"
+		modes[0].classList.remove("modeClicked");
+		
+		modes[1].classList.remove("modeClicked");
+		modes[2].classList.remove("modeClicked");
+		modes[0].disabled = true
+		modes[1].disabled = true
+		modes[2].disabled = true
+		this.classList.add("modeClicked");
+		this.disabled = false
+		if(this.textContent === "Heaven Mode"){
+			
+			var step = 10
+			
+			window.step = step 
+			init() ;
+
+		} else if(this.textContent === "Normal Mode"){
+		
+			var step = 2
+			window.step = step 
+			
+			init() ;
+
+		}else{
+			
+			var step = 1
+			window.step = step 
+			
+			init() ;
+
+		}
+		//this.textContent === "Easy" ?	numSquares = 3 : numSquares = 6;
+		
+	});
+}
+
 
 
 
 function init() {
+	
+
 
      randomColor()
      adjustSquare() 
@@ -41,24 +87,24 @@ function init() {
 
 function adjustColor(level) {
     
-	if(window.r > 255 - (level * step)){
+	if(window.r > 255 - (level * window.step)){
 
-		   window.ar = window.r - (level * step);}
+		   window.ar = window.r - (level * window.step);}
 		   else{
-		    	window.ar = window.r + (level * step)}
+		    	window.ar = window.r + (level * window.step)}
 
-		   if(window.g > 255 - (level * step)){
+		   if(window.g > 255 - (level * window.step)){
 
-		   window.ag = window.r - (level * step);}
+		   window.ag = window.r - (level * window.step);}
 		   else{
-		    	window.ag = window.g + (level * step)}
+		    	window.ag = window.g + (level * window.step)}
 
 
-		   if(window.b > 255 - (level * step)){
+		   if(window.b > 255 - (level * window.step)){
 
-		   window.ab = window.b - (level * step);}
+		   window.ab = window.b - (level * window.step);}
 		   else{
-		    	window.ab = window.b + (level * step)}
+		    	window.ab = window.b + (level * window.step)}
 
 		 return "rgb(" + window.ar + ", " + window.ag + ", " + window.ab + ")";		
            
@@ -92,6 +138,9 @@ function compareColor(){
 				
 			//for the check purpose: console.log(this.style.backgroundColor,  this.style.backgroundColor === window.targetColor)
 			score++
+			scoreshow = scoreshow + 5
+			
+			scoredisplay.innerHTML = scoreshow
 			reset()
 				
 
@@ -148,10 +197,14 @@ function endGame(){
     	squares[i].style.display = "none"
     }
 	
+	for (var i = 0; i < modes.length; i++){
+    	modes[i].disabled = true
+    }
+	
 
 	resultdiv.style.display = "block"
 
-	display.innerHTML = "Game Over!"
+	display.innerHTML = " "
 	result()
 
 }
